65ca5c2b7815aa98ae8f89cd7910c8e41ed468c6
ayshasinara <97454953+ayshasinara@users.noreply.github.com>
2022-04-19 23:08:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import System from './components/system';
+import Manufactureres from './components/manufactureres';
+function App() {
+  return (
+    <div className="App">
+
+      <System />
+      <Manufactureres />
+    </div>
+  );
+}
+
+export default App;
